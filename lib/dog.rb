88e8079bce57d2c48371ab9386fdcf7d755fb268
Juspher Mokua <juspermokua6@gmail.com>
2022-10-28 16:52:26 +0300

    done assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
-end
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end  
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end       
+ end
+ 
+pug =Dog.new
+pug.name="Pug"
+pointer =Dog.new
+pointer.breed="Pointer"
